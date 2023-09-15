dee4528354a871a6adfd2c3d7d85afd5fcc70a20
Ashwin Wilson <ashwinwilson24@gmail.com>
2023-09-15 17:00:11 +0530

    spelling mistakes and paddin errors solved, about section updated

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const typedText = document.querySelector('.typed-text');
-const textToType = "I'm a computer science Engineer";
+const textToType = "I'm an eager volunteer, a passionate and curious learner, driven to actively contribute to my community through the power of knowledge.";
 let index = 0;
 
 function typeText() {
   typedText.textContent = textToType.slice(0, index);
   index++;
 
-//   if (index > textToType.length) {
-//     index = 0;
-//   }
+  // if (index > textToType.length) {
+  //   index = 0;
+  // }
 
   setTimeout(typeText, 150); // Adjust typing speed here (in milliseconds)
 }
